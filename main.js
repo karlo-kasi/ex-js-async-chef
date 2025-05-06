c07ae295cb6618b6503381b8ca28fba7e2e14c7c
karlo-kasi <karlokasi1995@gmail.com>
2025-05-06 11:02:53 +0200

    fix

--- main.js
+++ main.js
@@ -10,35 +10,44 @@ Utilizzare await per chiamare le API.
 Restituire una Promise con la data di nascita dello chef.
 Gestire gli errori con try/catch*/
 
-async function getJSON(url){
-    const resp = await fetch(url)
-    const obj = await resp.json()
-    return obj
-}
+// async function getJSON(url){
+//     const resp = await fetch(url)
+//     const obj = await resp.json()
+//     return obj
+// }
 
 async function getChefBirthday(id) {
 
     
-    let chefBirthday
+    let formattedData
     try {
-        const resp = await getJSON(`https://dummyjson.com/recipes/${id}`)
-        let userId = await resp.userId
-        console.log(userId)
+        const resp = await fetch(`https://dummyjson.com/recipes/${id}`)
+        let user = await resp.json()
+        console.log(user.userId)
 
-        const chef = await getJSON(`https://dummyjson.com/users/${userId}`)
-        chefBirthday = chef.birthDate
-        console.log(chefBirthday)
+        const chefResponse = await fetch(`https://dummyjson.com/users/${user.userId}`)
+        const chef = chefResponse.json()
+        formattedData = dayjs(chef.birthDate).format('DD/MM/YYYY') 
 
     } catch (error) {
         console.error(`L' errore catturato è ${error}`)
     }
 
-    return chefBirthday
+    return formattedData
 
     
 }
 
 
-getChefBirthday(1)
-  .then(birthday => console.log("Data di nascita dello chef:", birthday))
-  .catch(error => console.error("Errore:", error.message));
+// getChefBirthday(1)
+//   .then(birthday => console.log("Data di nascita dello chef:", birthday))
+//   .catch(error => console.error("Errore:", error.message));
+
+(async () => {
+    try {
+        const birthday = await getChefBirthday(1)
+        console.log(`La data di Nascita è: ${birthday}`)
+    } catch (err){
+        console.error(err)
+    }
+})()
